33a366eb35c28cc8ca5b286217e13b13d805300a
im-world <52660201+im-world@users.noreply.github.com>
2020-11-07 16:23:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import shutil
 import time
 
 
-time = #expected download time
-backupTime = #backup after every ? seconds
+time = 60           #expected download time in minutes
+backupTime = 2      #backup after every ? minutes
 
 
-iterations = time/(backupTime) + 1 
+iterations = (time)/(backupTime) + 1 
 
 for i in range(iterations): 
 
-    original = r'C:\Users\ASUS\Downloads\Unconfirmed 174731.crdownload' #path of original crdownload file
-    backup = r'C:\Users\ASUS\Desktop\Academia\Unconfirmed 174731.crdownload' #path of backup file
+    original = r'C:\Users\ASUS\Downloads\Unconfirmed 174731.crdownload'          #path of original crdownload file
+    backup = r'C:\Users\ASUS\Desktop\Academia\Unconfirmed 174731.crdownload'     #path of backup file
 
     shutil.copyfile(original, backup)
-    time.sleep(backupTime)
+    time.sleep(backupTime*60)

5f1a2aaf0d2f5e1fa632df268c21cd83bef338f2
im-world <srathi61@gmail.com>
2022-01-11 20:19:57 +0530

    Rearranged code
    Automatic search for the crdownload file; variables created with the
    intent of fetching values from the user using an interface

--- main.py
+++ main.py
@@ -3,18 +3,25 @@
 
 import shutil
 import time
+import glob, os
 
 
 time = 60           #expected download time in minutes
 backupTime = 2      #backup after every ? minutes
-
+downloadsPath = ''
+backupPath = ''
+filename = ''
 
 iterations = (time)/(backupTime) + 1 
 
+os.chdir(downloadsPath)
+for file in glob.glob("*.crdownload"):
+    filename = file
+
+original = str(downloadsPath + filename)            #path of original crdownload file
+backup = str(backupPath + filename)                 #path of backup file
+
+
 for i in range(iterations): 
-
-    original = r'C:\Users\ASUS\Downloads\Unconfirmed 174731.crdownload'          #path of original crdownload file
-    backup = r'C:\Users\ASUS\Desktop\Academia\Unconfirmed 174731.crdownload'     #path of backup file
-
     shutil.copyfile(original, backup)
     time.sleep(backupTime*60)
